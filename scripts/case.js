81e176518b966490e0fada008b9033593ea40405
femmebot <femmebot@gmail.com>
2023-08-27 18:48:52 -0400

    node bg color script works!

--- scripts/case.js
+++ scripts/case.js
@@ -1,10 +1,15 @@
 const coloredBoxes = document.querySelectorAll('.colorbox');
+const nodeImageElements = document.querySelectorAll('.node-image');
 
 window.addEventListener('scroll', () => {
 	coloredBoxes.forEach((box) => {
 		if (box.getBoundingClientRect().top <= document.body.scrollTop) {
-            console.log(box.dataset.color);
+			// console.log(box.dataset.color);
 			document.body.style.backgroundColor = box.dataset.color;
+			nodeImageElements.forEach((node) => {
+				// console.log(node);
+				node.style.backgroundColor = box.dataset.color;
+			});
 		}
 	});
 });
